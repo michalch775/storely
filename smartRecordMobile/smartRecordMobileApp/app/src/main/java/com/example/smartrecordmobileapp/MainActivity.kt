39c79c33d7cbdcb57a7809f6d18533d1d97924da
DominikAndrzej <dominik.switlik@gmail.com>
2021-12-30 19:54:02 +0100

    scanning activity added

--- smartRecordMobile/smartRecordMobileApp/app/src/main/java/com/example/smartrecordmobileapp/MainActivity.kt
+++ smartRecordMobile/smartRecordMobileApp/app/src/main/java/com/example/smartrecordmobileapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartrecordmobileapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.smartrecordmobileapp.databinding.ActivityMainBinding
+import com.example.smartrecordmobileapp.ui.scanning.Scan
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +34,13 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val btnGoToScan = findViewById<Button>(R.id.btnGoToScan)
+
+        btnGoToScan.setOnClickListener {
+            val Intent = Intent(this, Scan::class.java)
+            startActivity(Intent)
+        }
+        //działa
     }
 }
